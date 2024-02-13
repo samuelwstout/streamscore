29d949795c434b7eb31f207d655c51f4d0d658da
Sam Stout <samuelwstout@gmail.com>
2024-02-12 19:29:29 -0600

    build simple input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,31 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Home() {
+  const [input, setInput] = useState("");
+
+  const handleSubmit = () => {
+    console.log(input);
+    setInput("");
+  };
+
   return (
-    <main className="min-h-screen w-full flex items-center justify-center">
-      <div>
-        <iframe
-          src="https://giphy.com/embed/3oz8xtBx06mcZWoNJm"
-          className="w-full aspect-auto"
-          allowFullScreen
-        ></iframe>
-        <div className="p-10 flex flex-col items-center justify-center gap-5">
-          <p>
-            Hey! I&apos;m building an AI tool for music education called
-            Streamscore.
-          </p>
-          <p>
-            As you can see, I&apos;m busy building and plan to have a first
-            version out soon :)
-          </p>
-        </div>
+    <main>
+      <div className="min-h-screen w-full flex flex-col justify-start items-center pt-5 gap-2">
+        <input
+          type="text"
+          className="rounded p-2 text-black"
+          placeholder="Enter text here"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") handleSubmit();
+          }}
+        />
+        <button onClick={handleSubmit} className="border p-2 rounded">
+          Submit
+        </button>
       </div>
     </main>
   );
