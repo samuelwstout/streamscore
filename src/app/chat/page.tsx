98a86e6784c445b00e3e3edd9e640b0e852988f7
Sam Stout <samuelwstout@gmail.com>
2024-02-25 22:33:14 -0600

    colors/styling

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -4,19 +4,20 @@ import { useChat } from "ai/react";
 
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
+
   return (
     <main className="h-screen flex">
-      <div className="w-1/6 flex flex-col">
-        <div className="border border-black h-20 flex items-center px-3">
+      <div className="w-1/6 flex flex-col bg-gray-50">
+        <div className="h-20 flex items-center px-3">
           <h1>Streamscore</h1>
         </div>
         <div className="flex-1"></div>
-        <div className="border border-black h-20 flex items-center px-3">
+        <div className="h-20 flex items-center px-3">
           <UserButton afterSignOutUrl="/" />
         </div>
       </div>
-      <div className="border border-black flex-1">
-        <div className="h-4/5">
+      <div className="flex-1">
+        <div className="h-5/6">
           <div className="h-full overflow-scroll">
             {messages.map((m) => (
               <div key={m.id} className="whitespace-pre-wrap">
@@ -28,12 +29,12 @@ export default function Chat() {
         </div>
         <form
           onSubmit={handleSubmit}
-          className="h-1/5 flex items-center justify-between px-4"
+          className="h-1/6 flex items-center justify-center"
         >
           <input
             type="text"
             value={input}
-            className="w-full h-10 px-2 border-2 border-gray-300 rounded-md"
+            className="w-1/2 h-10 px-2 border-2 border-gray-300 rounded-md"
             placeholder="Type your message here..."
             onChange={handleInputChange}
           />
