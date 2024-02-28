10b9f50c0443b42072ff3d5ef3ce7b14e44be339
Sam Stout <samuelwstout@gmail.com>
2024-02-28 14:51:56 -0600

    build layout with fixed divs and scrollable section

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -3,43 +3,45 @@ import { UserButton } from "@clerk/nextjs";
 import { useChat } from "ai/react";
 
 export default function Chat() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat();
+  // const { messages, input, handleInputChange, handleSubmit } = useChat();
 
   return (
-    <main className="h-screen flex">
-      <div className="w-1/6 flex flex-col bg-gray-50">
+    <main className="h-full">
+      <div className="w-1/6 flex flex-col bg-gray-50 h-screen fixed">
         <div className="h-20 flex items-center px-3">
           <h1>Streamscore</h1>
         </div>
-        <div className="flex-1"></div>
+        <div className="flex-1 flex flex-col"></div>
         <div className="h-20 flex items-center px-3">
           <UserButton afterSignOutUrl="/" />
         </div>
       </div>
-      <div className="flex-1">
-        <div className="h-5/6">
-          <div className="h-full overflow-scroll">
-            {messages.map((m) => (
-              <div key={m.id} className="whitespace-pre-wrap">
-                {m.role === "user" ? "User: " : "AI: "}
-                {m.content}
-              </div>
-            ))}
-          </div>
-        </div>
+      <div className="w-5/6 ml-auto flex flex-col justify-between">
+        <div className="py-10"></div>
+        <div className="px-10"></div>
+        <div className="py-12"></div>
         <form
-          onSubmit={handleSubmit}
-          className="h-1/6 flex items-center justify-center"
+          // onSubmit={handleSubmit}
+          className="flex justify-center fixed bottom-0 w-5/6 py-4"
         >
           <input
             type="text"
-            value={input}
+            // value={input}
             className="w-1/2 h-10 px-2 border-2 border-gray-300 rounded-md"
             placeholder="Type your message here..."
-            onChange={handleInputChange}
+            // onChange={handleInputChange}
           />
         </form>
       </div>
     </main>
   );
+}
+
+{
+  /* {messages.map((m) => (
+              <div key={m.id} className="whitespace-pre-wrap">
+                {m.role === "user" ? "User: " : "AI: "}
+                {m.content}
+              </div>
+            ))} */
 }
