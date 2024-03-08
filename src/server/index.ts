2595fc9200075a7abab81128ec1c10a3d88b6940
Sam Stout <samuelwstout@gmail.com>
2024-03-07 23:11:17 -0600

    set up route.ts and router with example procedure

---
+++ src/server/index.ts
@@ -0,0 +1,9 @@
+import { router, publicProcedure } from "./trpc";
+
+export const appRouter = router({
+  getTodos: publicProcedure.query(async () => {
+    return [10, 20, 30];
+  }),
+});
+
+export type AppRouter = typeof appRouter;
