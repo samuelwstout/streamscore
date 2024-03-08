eabb4bb7ff98d8233aa7beb0bf1cef9728847509
Sam Stout <samuelwstout@gmail.com>
2024-03-07 23:46:26 -0600

    setup for procedure build

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,8 @@
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import { migrate } from "drizzle-orm/better-sqlite3/migrator";
 import Database from "better-sqlite3";
+import { clerkClient } from "@clerk/nextjs/server";
+import { z } from "zod";
 
 import { router, publicProcedure } from "./trpc";
 
@@ -11,6 +13,9 @@ const db = drizzle(sqlite);
 
 migrate(db, { migrationsFolder: "drizzle" });
 
-export const appRouter = router({});
+export const appRouter = router({
+  // get all conversations for current user
+  // create conversation
+});
 
 export type AppRouter = typeof appRouter;
