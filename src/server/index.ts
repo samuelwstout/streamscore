9aa0ddbb36f294b9524522f882b69512cf1a380a
Sam Stout <samuelwstout@gmail.com>
2024-03-07 23:36:09 -0600

    connect db to trpc

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,16 @@
+import { drizzle } from "drizzle-orm/better-sqlite3";
+import { migrate } from "drizzle-orm/better-sqlite3/migrator";
+import Database from "better-sqlite3";
+
 import { router, publicProcedure } from "./trpc";
 
-export const appRouter = router({
-  getTodos: publicProcedure.query(async () => {
-    return [10, 20, 30];
-  }),
-});
+import { conversations } from "@/db/schema";
+
+const sqlite = new Database(process.env.TURSO_CONNECTION_URL);
+const db = drizzle(sqlite);
+
+migrate(db, { migrationsFolder: "drizzle" });
+
+export const appRouter = router({});
 
 export type AppRouter = typeof appRouter;
