c179d2119832e6664430c512d1f59201bc69d527
Sam Stout <samuelwstout@gmail.com>
2024-05-15 22:06:54 -0500

    add list with ellipsis/popup. will need further styling

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,16 +1,8 @@
 "use client";
 import { Fragment, useState, useEffect } from "react";
-import { Dialog, Transition } from "@headlessui/react";
-import {
-  Bars3Icon,
-  CalendarIcon,
-  ChartPieIcon,
-  DocumentDuplicateIcon,
-  FolderIcon,
-  HomeIcon,
-  UsersIcon,
-  XMarkIcon,
-} from "@heroicons/react/24/outline";
+import { Dialog, Transition, Menu } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { EllipsisVerticalIcon } from "@heroicons/react/20/solid";
 import { UserButton, useUser } from "@clerk/nextjs";
 import type { SelectConversation } from "@/db/schema";
 
@@ -147,15 +139,15 @@ export default function Chat() {
         {/* Static sidebar for desktop */}
         <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
           {/* Sidebar component, swap this element with another sidebar if you like */}
-          <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6">
-            <div className="flex h-16 shrink-0 items-center">
+          <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900">
+            <div className="flex h-16 shrink-0 items-center sticky top-0 bg-gray-900 z-10 px-6">
               <img
                 className="h-8 w-auto"
                 src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                alt="Your Company"
+                alt="StreamScore"
               />
             </div>
-            <nav className="flex flex-1 flex-col">
+            <nav className="flex flex-1 flex-col px-6">
               <ul role="list" className="flex flex-1 flex-col gap-y-7">
                 <li>
                   <ul role="list" className="-mx-2 space-y-1">
@@ -167,14 +159,52 @@ export default function Chat() {
                     {conversations
                       .sort((a, b) => b.id - a.id)
                       .map((item) => (
-                        <li key={item.id}>
-                          <div
-                            className={classNames(
-                              "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold",
-                              "text-gray-400 hover:text-white hover:bg-gray-800"
-                            )}
-                          >
-                            {item.title}
+                        <li
+                          key={item.id}
+                          className="flex items-center justify-between gap-x-6 py-5 bg-white"
+                        >
+                          <div className="min-w-0">
+                            <div className="flex items-start gap-x-3">
+                              <p className="text-sm font-semibold leading-6 text-gray-900 truncate">
+                                {item.title}
+                              </p>
+                            </div>
+                          </div>
+                          <div className="flex flex-none items-center gap-x-4">
+                            <Menu as="div" className="relative flex-none">
+                              <Menu.Button className="-m-2.5 block p-2.5 text-gray-500 hover:text-gray-900">
+                                <span className="sr-only">Open options</span>
+                                <EllipsisVerticalIcon
+                                  className="h-5 w-5"
+                                  aria-hidden="true"
+                                />
+                              </Menu.Button>
+                              <Transition
+                                as={Fragment}
+                                enter="transition ease-out duration-100"
+                                enterFrom="transform opacity-0 scale-95"
+                                enterTo="transform opacity-100 scale-100"
+                                leave="transition ease-in duration-75"
+                                leaveFrom="transform opacity-100 scale-100"
+                                leaveTo="transform opacity-0 scale-95"
+                              >
+                                <Menu.Items className="absolute right-0 z-10 mt-2 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
+                                  <Menu.Item>
+                                    {({ active }) => (
+                                      <a
+                                        href="#"
+                                        className={classNames(
+                                          active ? "bg-gray-50" : "",
+                                          "block px-3 py-1 text-sm leading-6 text-gray-900"
+                                        )}
+                                      >
+                                        Delete
+                                      </a>
+                                    )}
+                                  </Menu.Item>
+                                </Menu.Items>
+                              </Transition>
+                            </Menu>
                           </div>
                         </li>
                       ))}
