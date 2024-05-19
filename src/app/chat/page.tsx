690cfb3a69c41713889d999e8489cd58d115c4a0
Sam Stout <samuelwstout@gmail.com>
2024-05-18 19:05:16 -0500

    style sidebar items

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -107,21 +107,57 @@ export default function Chat() {
                         <li>
                           <ul role="list" className="-mx-2 space-y-1">
                             {isLoadingConversations && (
-                              <div className="text-sm text-white leading-6 font-semibold">
+                              <div className="text-sm text-gray-400 leading-6 font-semibold">
                                 Loading conversations...
                               </div>
                             )}
                             {conversations
                               .sort((a, b) => b.id - a.id)
                               .map((item) => (
-                                <li key={item.id}>
-                                  <div
-                                    className={classNames(
-                                      "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold",
-                                      "text-gray-400 hover:text-white hover:bg-gray-800"
-                                    )}
-                                  >
+                                <li
+                                  key={item.id}
+                                  className="flex items-center justify-between gap-x-3 py-3 hover:bg-gray-800 rounded-md group cursor-pointer"
+                                >
+                                  <div className="group-hover:text-white group flex text-sm leading-6 font-semibold truncate text-gray-400 px-2">
                                     {item.title}
+                                  </div>
+                                  <div className="flex flex-none items-center gap-x-4">
+                                    <Menu
+                                      as="div"
+                                      className="relative flex-none"
+                                    >
+                                      <Menu.Button className="-m-2.5 block p-2.5 text-gray-500 hover:text-white">
+                                        <EllipsisVerticalIcon
+                                          className="h-5 w-5"
+                                          aria-hidden="true"
+                                        />
+                                      </Menu.Button>
+                                      <Transition
+                                        as={Fragment}
+                                        enter="transition ease-out duration-100"
+                                        enterFrom="transform opacity-0 scale-95"
+                                        enterTo="transform opacity-100 scale-100"
+                                        leave="transition ease-in duration-75"
+                                        leaveFrom="transform opacity-100 scale-100"
+                                        leaveTo="transform opacity-0 scale-95"
+                                      >
+                                        <Menu.Items className="absolute right-0 z-10 mt-2 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
+                                          <Menu.Item>
+                                            {({ active }) => (
+                                              <a
+                                                href="#"
+                                                className={classNames(
+                                                  active ? "bg-gray-50" : "",
+                                                  "block px-3 py-1 text-sm leading-6 text-gray-900"
+                                                )}
+                                              >
+                                                Delete
+                                              </a>
+                                            )}
+                                          </Menu.Item>
+                                        </Menu.Items>
+                                      </Transition>
+                                    </Menu>
                                   </div>
                                 </li>
                               ))}
@@ -152,7 +188,7 @@ export default function Chat() {
                 <li>
                   <ul role="list" className="-mx-2 space-y-1">
                     {isLoadingConversations && (
-                      <div className="text-sm text-white leading-6 font-semibold">
+                      <div className="text-sm text-gray-400 leading-6 font-semibold">
                         Loading conversations...
                       </div>
                     )}
@@ -161,19 +197,14 @@ export default function Chat() {
                       .map((item) => (
                         <li
                           key={item.id}
-                          className="flex items-center justify-between gap-x-6 py-5 bg-white"
+                          className="flex items-center justify-between gap-x-3 py-3 hover:bg-gray-800 rounded-md group cursor-pointer"
                         >
-                          <div className="min-w-0">
-                            <div className="flex items-start gap-x-3">
-                              <p className="text-sm font-semibold leading-6 text-gray-900 truncate">
-                                {item.title}
-                              </p>
-                            </div>
+                          <div className="group-hover:text-white group flex text-sm leading-6 font-semibold truncate text-gray-400 px-2">
+                            {item.title}
                           </div>
                           <div className="flex flex-none items-center gap-x-4">
                             <Menu as="div" className="relative flex-none">
-                              <Menu.Button className="-m-2.5 block p-2.5 text-gray-500 hover:text-gray-900">
-                                <span className="sr-only">Open options</span>
+                              <Menu.Button className="-m-2.5 block p-2.5 text-gray-500 hover:text-white">
                                 <EllipsisVerticalIcon
                                   className="h-5 w-5"
                                   aria-hidden="true"
