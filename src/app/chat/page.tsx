420bd35943470ddd63c9249cd0defd252fcec50c
Sam Stout <samuelwstout@gmail.com>
2024-05-27 22:32:00 -0500

    setup delete modal and functionality

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,6 +1,13 @@
 "use client";
 import { Fragment, useState, useEffect } from "react";
-import { Dialog, Transition, Menu } from "@headlessui/react";
+import {
+  Dialog,
+  Transition,
+  Menu,
+  DialogTitle,
+  DialogPanel,
+  TransitionChild,
+} from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { EllipsisVerticalIcon } from "@heroicons/react/20/solid";
 import { UserButton, useUser } from "@clerk/nextjs";
@@ -8,6 +15,15 @@ import type { SelectConversation } from "@/db/schema";
 import { autoResize } from "@/utils/autoResizeInput";
 import { useChat } from "ai/react";
 import Image from "next/image";
+import type { Message } from "ai";
+import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
+
+interface ClickedConvProps {
+  id: number;
+  userId: string | null;
+  title: string | null;
+  messages: Message[] | null;
+}
 
 function classNames(...classes: any[]) {
   return classes.filter(Boolean).join(" ");
@@ -20,6 +36,8 @@ export default function Chat() {
   const [isNewConversation, setIsNewConversation] = useState(true);
   const [chatFinished, setChatFinished] = useState(false);
   const [conversationId, setConversationId] = useState<null | number>(null);
+  const [clickedConv, setClickedConv] = useState<null | ClickedConvProps>(null);
+  const [deleteModal, setDeleteModal] = useState(false);
 
   const { user } = useUser();
 
@@ -100,6 +118,25 @@ export default function Chat() {
     }
   }
 
+  async function deleteConversation(clickedConv: ClickedConvProps) {
+    const updatedConversations = conversations.filter(
+      (i) => i.id !== clickedConv.id
+    );
+    setConversations(updatedConversations);
+    const response = await fetch(`api/deleteConversation/${clickedConv.id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (!response.ok) {
+      throw new Error("Failed to delete conversation.");
+    } else {
+      setDeleteModal(false);
+      startNewConversation();
+    }
+  }
+
   function getMessages(clickId: number) {
     setSidebarOpen(false);
     setIsNewConversation(false);
@@ -129,6 +166,77 @@ export default function Chat() {
         <body class="h-full">
         ```
       */}
+      <Transition show={deleteModal}>
+        <Dialog className="relative z-10" onClose={setDeleteModal}>
+          <TransitionChild
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </TransitionChild>
+
+          <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+              <TransitionChild
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                enterTo="opacity-100 translate-y-0 sm:scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              >
+                <DialogPanel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+                  <div className="sm:flex sm:items-start">
+                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                      <ExclamationTriangleIcon
+                        className="h-6 w-6 text-red-600"
+                        aria-hidden="true"
+                      />
+                    </div>
+                    <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                      <DialogTitle
+                        as="h3"
+                        className="text-base font-semibold leading-6 text-gray-900"
+                      >
+                        Delete conversation?
+                      </DialogTitle>
+                      <div className="mt-2">
+                        <p className="text-sm text-gray-500">
+                          This will delete{" "}
+                          <span className="font-semibold">
+                            {clickedConv?.title}
+                          </span>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+                    <button
+                      type="button"
+                      className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                      onClick={() => deleteConversation(clickedConv!)}
+                    >
+                      Delete
+                    </button>
+                    <button
+                      type="button"
+                      className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                      onClick={() => setDeleteModal(false)}
+                      data-autofocus
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                </DialogPanel>
+              </TransitionChild>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
       <div>
         <Transition.Root show={sidebarOpen} as={Fragment}>
           <Dialog className="relative z-50 lg:hidden" onClose={setSidebarOpen}>
@@ -238,15 +346,17 @@ export default function Chat() {
                                         <Menu.Items className="absolute right-0 z-10 mt-2 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
                                           <Menu.Item>
                                             {({ active }) => (
-                                              <a
-                                                href="#"
+                                              <div
+                                                onClick={() =>
+                                                  setDeleteModal(true)
+                                                }
                                                 className={classNames(
                                                   active ? "bg-gray-50" : "",
                                                   "block px-3 py-1 text-sm leading-6 text-gray-900"
                                                 )}
                                               >
                                                 Delete
-                                              </a>
+                                              </div>
                                             )}
                                           </Menu.Item>
                                         </Menu.Items>
@@ -325,15 +435,18 @@ export default function Chat() {
                                 <Menu.Items className="absolute right-0 z-10 mt-2 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
                                   <Menu.Item>
                                     {({ active }) => (
-                                      <a
-                                        href="#"
+                                      <div
+                                        onClick={() => {
+                                          setDeleteModal(true);
+                                          setClickedConv(item);
+                                        }}
                                         className={classNames(
                                           active ? "bg-gray-50" : "",
                                           "block px-3 py-1 text-sm leading-6 text-gray-900"
                                         )}
                                       >
                                         Delete
-                                      </a>
+                                      </div>
                                     )}
                                   </Menu.Item>
                                 </Menu.Items>
