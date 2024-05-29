4cb43851462b6c34db75e0d10bcd1afbf22cf3db
Sam Stout <samuelwstout@gmail.com>
2024-05-29 15:54:58 -0500

    add clickedConv to mobile sidebar

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -356,16 +356,14 @@ export default function Chat() {
                                         leaveFrom="transform opacity-100 scale-100"
                                         leaveTo="transform opacity-0 scale-95"
                                       >
-                                        <Menu.Items
-                                          className="absolute right-0 z-10 mt-2 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none"
-                                          // onClick={(e) => e.stopPropagation()}
-                                        >
+                                        <Menu.Items className="absolute right-0 z-10 mt-2 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
                                           <Menu.Item>
                                             {({ active }) => (
                                               <div
-                                                onClick={() =>
-                                                  setDeleteModal(true)
-                                                }
+                                                onClick={() => {
+                                                  setDeleteModal(true);
+                                                  setClickedConv(item);
+                                                }}
                                                 className={classNames(
                                                   active ? "bg-gray-50" : "",
                                                   "block px-3 py-1 text-sm leading-6 text-gray-900"
