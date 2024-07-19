27627c7889dbb7b65bc198fe1f11be406be4f056
Sam Stout <samuelwstout@gmail.com>
2024-07-18 23:24:40 -0500

    animate landing page and adjust copy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,14 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { Trail } from "./_components/Trail/Trail";
+
 export default function Landing() {
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    setOpen(true);
+  }, []);
+
   return (
     <div className="bg-white">
       <div className="relative isolate pt-14">
@@ -6,50 +16,28 @@ export default function Landing() {
           className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
           aria-hidden="true"
         ></div>
-        <div className="py-24 sm:py-32 lg:pb-40">
+        <div className="py-24 sm:py-32">
           <div className="mx-auto max-w-7xl px-6 lg:px-8">
             <div className="mx-auto max-w-2xl text-center">
-              <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                Welcome to Streamscore!
-              </h1>
-              <p className="mt-3 text-lg leading-8 text-gray-600">
-                Feel free to peek at my current progress building an LLM chat
-                interface dedicated to rendering music notation.
-              </p>
-              <a
-                href="https://www.linkedin.com/in/samwstout/"
-                className="text-lg leading-8 text-blue-600 hover:text-blue-800"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Connect with me on LinkedIn!
-              </a>
-              <div className="mt-3 flex items-center justify-center gap-x-6">
-                <a
-                  href="/chat"
-                  className="rounded-md bg-black px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
-                >
-                  Get started
-                </a>
-              </div>
+              <Trail open={open}>
+                <h1 className="text-6xl font-bold tracking-tight text-gray-900">
+                  Streamscore
+                </h1>
+                <h1 className="text-xl font-bold tracking-tight text-gray-900">
+                  an LLM chat interface that renders sheet music.
+                </h1>
+                <div className="flex items-center justify-center">
+                  <a
+                    href="/chat"
+                    className="rounded-md bg-black px-3.5 py-2.5 text-sm font-semibold tracking-tight text-white shadow-sm hover:bg-gray-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+                  >
+                    Get started
+                  </a>
+                </div>
+              </Trail>
             </div>
-            {/* <div className="mt-16 flow-root sm:mt-24">
-              <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
-                <img
-                  src="https://tailwindui.com/img/component-images/project-app-screenshot.png"
-                  alt="App screenshot"
-                  width={2432}
-                  height={1442}
-                  className="rounded-md shadow-2xl ring-1 ring-gray-900/10"
-                />
-              </div>
-            </div> */}
           </div>
         </div>
-        <div
-          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-          aria-hidden="true"
-        ></div>
       </div>
     </div>
   );
