3d1b5e4834fb0a5fb758abb425536d3a3fe3e3d8
Sam Stout <samuelwstout@gmail.com>
2024-07-19 00:55:12 -0500

    adjust landing page copy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,18 +20,18 @@ export default function Landing() {
           <div className="mx-auto max-w-7xl lg:px-8">
             <div className="mx-auto max-w-2xl text-center">
               <Trail open={open}>
-                <h1 className="text-5xl lg:text-6xl font-bold tracking-tight text-gray-900">
-                  Streamscore
+                <h1 className="text-5xl lg:text-6xl font-semibold tracking-tight text-gray-900">
+                  streamscore
                 </h1>
-                <h1 className="text-base lg:text-xl font-bold tracking-tight text-gray-900">
-                  an LLM chat interface that renders sheet music.
+                <h1 className="text-base lg:text-xl font-normal tracking-tight text-gray-900">
+                  generative AI for sheet music
                 </h1>
                 <div className="flex items-center justify-center">
                   <a
                     href="/chat"
-                    className="rounded-md bg-black px-3.5 py-2.5 text-sm font-semibold tracking-tight text-white shadow-sm hover:bg-gray-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+                    className="rounded-md bg-black px-3.5 py-2.5 text-sm font-normal tracking-tight text-white shadow-sm hover:bg-gray-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
                   >
-                    Get started
+                    get started
                   </a>
                 </div>
               </Trail>
