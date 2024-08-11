5fd0525e7cb375948071de2e368fb7d5a151e670
Sam Stout <samuelwstout@gmail.com>
2024-08-11 16:00:41 -0500

    add trail animation back

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,23 +23,25 @@ export default function Landing() {
         <div className="py-24 sm:py-32">
           <div className="mx-auto max-w-7xl lg:px-8">
             <div className="mx-auto max-w-3xl text-center">
-              <h1 className="text-5xl lg:text-6xl font-semibold tracking-tight text-gray-900">
-                streamscore
-              </h1>
-              <h1 className="text-base lg:text-xl font-normal tracking-tight text-gray-900">
-                generative AI for sheet music
-              </h1>
+              <Trail open={open}>
+                <h1 className="text-5xl lg:text-6xl font-semibold tracking-tight text-gray-900">
+                  streamscore
+                </h1>
+                <h1 className="text-base lg:text-xl font-normal tracking-tight text-gray-900">
+                  generative AI for sheet music
+                </h1>
 
-              <ABCNotation content={abcContent} />
+                <ABCNotation content={abcContent} />
 
-              <div className="flex items-center justify-center mt-8">
-                <a
-                  href="/chat"
-                  className="rounded-md bg-black px-3.5 py-2.5 text-sm font-normal tracking-tight text-white shadow-sm hover:bg-gray-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
-                >
-                  get started
-                </a>
-              </div>
+                <div className="flex items-center justify-center mt-8">
+                  <a
+                    href="/chat"
+                    className="rounded-md bg-black px-3.5 py-2.5 text-sm font-normal tracking-tight text-white shadow-sm hover:bg-gray-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+                  >
+                    get started
+                  </a>
+                </div>
+              </Trail>
             </div>
           </div>
         </div>
